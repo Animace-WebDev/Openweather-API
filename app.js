fe40a9db2d36c1a2b1737994ff2cd1da073ff81f
Animesh <animesh.kashyap@student.lab.fi>
2021-05-04 10:27:27 +0300

    I commit.

---
+++ app.js
@@ -0,0 +1,25 @@
+let input = document.querySelector('.input_text');
+let main = document.querySelector('#name');
+let temp = document.querySelector('.temp');
+let desc = document.querySelector('.desc');
+let clouds = document.querySelector('.clouds');
+let button= document.querySelector('.submit');
+
+
+button.addEventListener('click', function(name){
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&appid=847632d6d69074364cc015bfc7ce08d3')
+.then(response => response.json())
+.then(data => {
+  var tempValue = data['main']['temp'];
+  var nameValue = data['name'];
+  var descValue = data['weather'][0]['description'];
+
+  main.innerHTML = nameValue;
+  temp.innerHTML = "Temp - "+tempValue;
+  desc.innerHTML = "desc - "+descValue;
+  input.value ="";
+
+})
+
+.catch(err => alert("No such city!"));
+})
